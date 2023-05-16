5751b07d758a0c3d0c31c34603491ade02908b27
Ritam Guha <ritamguha16gmail.com>
2023-05-16 19:21:24 -0400

    added initial dashboard

--- regemo/algorithm/dashboard.py
+++ regemo/algorithm/dashboard.py
@@ -17,13 +17,13 @@ from pdf2image import convert_from_path
 app = dash.Dash(__name__)
 
 
-def convert_pdf_to_image(path):
-    images = convert_from_path(path)
-    for i in range(len(images)):
-        images[i].save(f"/home/ritz/projects/coin_lab/Research_Directions/EC/regemo/regemo/results/crashworthiness/regularity_pf_long.jpg", 'JPEG')
-
-
-def convert_image_to_go_obj(image_path):
+# def convert_pdf_to_image(path):
+#     images = convert_from_path(path)
+#     for i in range(len(images)):
+#         images[i].save(f"/home/ritz/projects/coin_lab/Research_Directions/EC/regemo/regemo/results/crashworthiness/regularity_pf_long.jpg", 'JPEG')
+#
+#
+# def convert_image_to_go_obj(image_path):
 
 
 
@@ -48,7 +48,7 @@ def create_original_scatter_plots(selected_value):
     elif df_orig_F.shape[1] == 3:
         fig_scatter = go.Figure(
             data=go.Scatter3d(x=df_orig_F["F0"], y=df_orig_F["F1"], z=df_orig_F["F2"], mode='markers',
-                              marker=dict(color='red', line=dict(color='black', width=1))))
+                              marker=dict(color='red', line=dict(color='white', width=1))))
         fig_scatter.update_layout(scene=dict(xaxis_title='F0',
                                              yaxis_title='F1',
                                              zaxis_title='F2'))
@@ -122,23 +122,23 @@ def create_reg_scatter_plots(selected_value):
     return fig_pcp
 
 
-@app.callback(
-    Output('regular_pcp', 'figure'),
-    [Input('dropdown_problem', 'value')]
-)
-def create_reg_scatter_plots(selected_value):
-    data_reg = pickle.load(
-        open(f"{config.BASE_PATH}/results/{selected_value}/final_regular_population.pickle", "rb"))
-    df_reg_X = pd.DataFrame(data_reg["X"], columns=[f"X{i}" for i in range(data_reg["X"].shape[1])])
-
-    # PCP
-    fig_pcp = go.Figure(data=go.Parcoords(
-        line=dict(color="blue"),
-        dimensions=list([dict(label=f'X{i}',
-                              values=df_reg_X[f'X{i}']) for i in range(df_reg_X.shape[1])])))
-    fig_pcp.update_layout(title="Parallel Coordinate Plot for the Regular Front", template="plotly_dark")
-
-    return fig_pcp
+# @app.callback(
+#     Output('regular_pcp', 'figure'),
+#     [Input('dropdown_problem', 'value')]
+# )
+# def create_reg_scatter_plots(selected_value):
+#     data_reg = pickle.load(
+#         open(f"{config.BASE_PATH}/results/{selected_value}/final_regular_population.pickle", "rb"))
+#     df_reg_X = pd.DataFrame(data_reg["X"], columns=[f"X{i}" for i in range(data_reg["X"].shape[1])])
+#
+#     # PCP
+#     fig_pcp = go.Figure(data=go.Parcoords(
+#         line=dict(color="blue"),
+#         dimensions=list([dict(label=f'X{i}',
+#                               values=df_reg_X[f'X{i}']) for i in range(df_reg_X.shape[1])])))
+#     fig_pcp.update_layout(title="Parallel Coordinate Plot for the Regular Front", template="plotly_dark")
+#
+#     return fig_pcp
 
 
 def main():
@@ -177,6 +177,6 @@ def main():
 
 if __name__ == '__main__':
     # convert_pdf_to_image("/home/ritz/projects/coin_lab/Research_Directions/EC/regemo/regemo/results/crashworthiness/regularity_pf_long.pdf")
-    convert_image_to_go_obj("/home/ritz/projects/coin_lab/Research_Directions/EC/regemo/regemo/results/crashworthiness/regularity_pf_long.jpg")
-    # main()
+    # convert_image_to_go_obj("/home/ritz/projects/coin_lab/Research_Directions/EC/regemo/regemo/results/crashworthiness/regularity_pf_long.jpg")
+    main()
     app.run_server(debug=True)
